0ab07b2402e9cca262c53b2603053ce0a054648a
Eugene Mironov <mir-evgenii@mail.ru>
2021-01-05 19:44:51 +0300

    Added video call.

--- webrtc.js
+++ webrtc.js
@@ -1,3 +1,13 @@
+// offer
+// videoWebRTC();
+// createOfferSDP();
+// start(answerSDP);
+
+// join
+// videoWebRTC();
+// createAnswerSDP(offerSDP);
+
+
 // offer/join
 var server = { urls: "stun:stun.l.google.com:19302"};
 var sdpConstraints = { optional: [{RtpDataChannels: true}]  };
@@ -6,35 +16,63 @@ var dc;
 var state; //статус соединения
 var sdp_msg; // SDP датаграмма
 
-function joinWebRTC() {
-  // join 1 
-  pc.ondatachannel  = function(e) {dc = e.channel; dcInit(dc)};
-  // join 2 sdp датаграмма
-  pc.onicecandidate = function(e) {
-    if (e.candidate) return;
-    sdp_msg = JSON.stringify(pc.localDescription);
-    console.log(sdp_msg);
-  }
-  // join 3 изменение статуса 
-  pc.oniceconnectionstatechange = function(e) {
-    state = pc.iceConnectionState;
-    console.log(state);
-  }
-  // join 4
-  function dcInit(dc) {
-    dc.onopen    = function()  {console.log("Conected!")};
-    dc.onmessage = function(e) {if (e.data) console.log(e.data); app.addMsg(e.data, app.frend.name, 'I am');}
-  }
-  // join 5
-  function createAnswerSDP(offerSDP) {
-    var offerDesc = new RTCSessionDescription(offerSDP);
-    pc.setRemoteDescription(offerDesc)
-    pc.createAnswer(function (answerDesc) {
-      pc.setLocalDescription(answerDesc)
-    }, function () {console.warn("Couldn't create offer")},
-    sdpConstraints);
-  }
+//---------video--------------
+
+function videoWebRTC(){
+  pc.ontrack = event => {
+    const stream = event.streams[0];
+    if (!remoteVideo.srcObject || remoteVideo.srcObject.id !== stream.id) {
+      remoteVideo.srcObject = stream;
+    }
+  };
+
+  navigator.mediaDevices.getUserMedia({
+    audio: true,
+    video: true,
+  }).then(stream => {
+    // Display your local video in #localVideo element
+    localVideo.srcObject = stream;
+    // Add your stream to be sent to the conneting peer
+    stream.getTracks().forEach(track => pc.addTrack(track, stream));
+  });
 }
+
+//----------join--------------
+
+// join 4
+// function dcInit(dc) {
+//   dc.onopen    = function()  {console.log("Conected!")};
+//   dc.onmessage = function(e) {if (e.data) console.log(e.data); app.addMsg(e.data, app.frend.name, 'I am');}
+// }
+
+function joinWebRTC() {
+    // join 1 
+    pc.ondatachannel = function(e) {dc = e.channel; dcInit(dc)};
+    // join 2 sdp датаграмма
+    pc.onicecandidate = function(e) {
+      if (e.candidate) return;
+      sdp_msg = JSON.stringify(pc.localDescription);
+      console.log(sdp_msg);
+    }
+    // join 3 изменение статуса 
+    pc.oniceconnectionstatechange = function(e) {
+      state = pc.iceConnectionState;
+      console.log(state);
+    }
+}
+
+// join 5
+function createAnswerSDP(offerSDP) {
+  joinWebRTC();
+  var offerDesc = new RTCSessionDescription(offerSDP);
+  pc.setRemoteDescription(offerDesc);
+  pc.createAnswer(function (answerDesc) {
+    pc.setLocalDescription(answerDesc)
+  }, function () {console.warn("Couldn't create offer")},
+  sdpConstraints);
+}
+
+//----------offer--------------
 
 function offerWebRTC() {
   // offer 1 изменение статуса 
@@ -48,33 +86,30 @@ function offerWebRTC() {
     sdp_msg = JSON.stringify(pc.localDescription);
     console.log(sdp_msg);
   }
-  // offer 3
-  function createOfferSDP() {
-    dc = pc.createDataChannel("chat");
-    pc.createOffer().then(function(e) {
-      pc.setLocalDescription(e)
-    });
-    dc.onmessage = function(e) {
-      if (e.data) console.log(e.data); app.addMsg(e.data, app.frend.name, 'I am'); //TODO добавление сообщения //addMSG(e.data, "other");
-    }
-  }
-  // offer 4
-  function start(answerSDP) {
-    var answerDesc = new RTCSessionDescription(answerSDP);
-    pc.setRemoteDescription(answerDesc);
-  }
-
-  createOfferSDP();
 }
 
+// offer 3
+function createOfferSDP() {
+  //videoWebRTC();
+  offerWebRTC();
+  // dc = pc.createDataChannel("chat"); // chat
+  pc.createOffer().then(function(e) {
+    pc.setLocalDescription(e)
+  });
+  // dc.onmessage = function(e) { // chat
+  //   if (e.data) console.log(e.data); app.addMsg(e.data, app.frend.name, 'I am'); //TODO добавление сообщения //addMSG(e.data, "other");
+  // }
+}
+
+// offer 4
+function start(answerSDP) {
+  var answerDesc = new RTCSessionDescription(answerSDP);
+  pc.setRemoteDescription(answerDesc);
+}
+
+//-----------------------------
 
 // offer/join отправка сообщения
 function sendMsgWebRtc (msg) {
   dc.send(msg);
 }
-
-
-
-
-
-
